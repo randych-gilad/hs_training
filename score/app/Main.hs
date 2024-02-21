af29fd4ea2c8cd2eada464209a567dcc73bc839b
Randych <59070097+randych-gilad@users.noreply.github.com>
2024-02-22 01:35:38 +0200

    construct table header

--- score/app/Main.hs
+++ score/app/Main.hs
@@ -1,4 +1,12 @@
 module Main where
+import Data.List (intercalate)
 
 main :: IO ()
-main = undefined
+main = putStrLn constructTable
+
+constructTable :: String
+constructTable =
+    do rowDelimiter ++ "\n"
+    ++ "|   " ++ "Name" ++ "    |" ++ "    Time" ++ "   |" ++ "   Diff" ++ "    |" ++ "    Rank" ++ "   |" ++ "\n"
+    ++ rowDelimiter
+  where rowDelimiter = intercalate "-----------" . replicate 5 $ "+"
