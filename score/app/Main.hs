cb7e87c702af4727691c2fd31f71bfc8ab584e09
Randych <59070097+randych-gilad@users.noreply.github.com>
2024-02-22 02:28:41 +0200

    contestants

--- score/app/Main.hs
+++ score/app/Main.hs
@@ -18,6 +18,16 @@ data Contestant = Contestant
   , time :: String
   , diff :: String
   , rank :: Rank
-  }
+  } deriving Show
 
-data Rank = Bronze | Silver | Golden deriving (Eq, Ord)
+data Rank = NA | Bronze | Silver | Golden deriving (Eq, Ord, Show)
+
+contestants :: [Contestant]
+contestants =
+  [
+    Contestant { name = "Kekusman", time = "5:00.00", diff = "+00:00.00", rank = Golden},
+    Contestant { name = "weewee63", time = "7:30.00", diff = "+2:30.00", rank = Silver},
+    Contestant { name = "LongAsfNickname", time = "8:01.00", diff = "+0:31.00", rank = Bronze},
+    Contestant { name = "Pewpewboi", time = "9:59.59", diff = "+1:58.59", rank = Bronze},
+    Contestant { name = "HowDoIplayThis", time = "10.01.00", diff = "+1:01.01", rank = NA}
+  ]
