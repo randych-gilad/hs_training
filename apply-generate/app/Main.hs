4073b5bccc1bae7b39c625995a0a1299bcbe81d3
Randych <59070097+randych-gilad@users.noreply.github.com>
2024-02-24 19:39:18 +0200

    add more constructors

--- apply-generate/app/Main.hs
+++ apply-generate/app/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main (main) where
@@ -7,13 +8,23 @@ import Data.Yaml
 -- import qualified Data.ByteString.Lazy.Char8 as BL
 import qualified Data.ByteString.Internal as BL
 import Manifest
+    ( Selector(matchLabels, Selector),
+      MatchLabels(app, MatchLabels),
+      Spec(selector, Spec, replicas, revisionHistoryLimit),
+      Metadata(name, Metadata, namespace),
+      Manifest(spec, Manifest, apiVersion, metadata, kind) )
 
 main :: IO ()
 main = do
-  let manifest =  Manifest {kind = "Deployment", 
-      metadata =  let namespace = "kekus"
-                      name = namespace ++ "-web"
-                  in Metadata {namespace, name}
-  , apiVersion = "apps/v1"}
+  let manifest    = Manifest { apiVersion = "apps/v1", 
+      metadata    = let namespace = "kekus"
+                        name = namespace ++ "-web"
+                    in Metadata { namespace, name }
+    , kind        = "Deployment"
+    , spec        = let replicas = 1
+                        revisionHistoryLimit = 2
+                        selector = Selector { matchLabels = MatchLabels { app = "kekus-web" } }
+                    in Spec { replicas, revisionHistoryLimit, selector }
+    }
   -- BL.putStrLn (Data.Yaml.encode manifest)
   putStrLn $ BL.unpackChars $ Data.Yaml.encode manifest
--- apply-generate/app/Manifest.hs
+++ apply-generate/app/Manifest.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+
 module Manifest where
+
 import GHC.Generics (Generic)
-import Data.List (isInfixOf)
 import Data.Aeson (ToJSON, toJSON, omitNothingFields, genericToJSON, defaultOptions)
 
 data Manifest = Manifest
@@ -8,12 +10,38 @@ data Manifest = Manifest
   apiVersion :: String
   , kind :: String
   , metadata :: Metadata
+  , spec :: Spec
   } deriving (Show, Generic)
 
 data Metadata = Metadata
   { name :: String
   , namespace :: String
+  } | MetadataTemplate
+  { 
+    labels :: MatchLabels
+  }
+  deriving (Show, Generic)
+
+data Spec = Spec
+  { replicas :: Int
+  , revisionHistoryLimit :: Int
+  , selector :: Selector
   } deriving (Show, Generic)
+
+
+newtype Selector = Selector
+  { matchLabels :: MatchLabels }
+  deriving (Show, Generic)
+
+newtype MatchLabels = MatchLabels
+  { app :: String }
+  deriving (Show, Generic)
+newtype Template = Template
+  { metadata :: Metadata }
 
 instance ToJSON Manifest where toJSON = genericToJSON defaultOptions { omitNothingFields  = True }
 instance ToJSON Metadata where toJSON = genericToJSON defaultOptions { omitNothingFields  = True }
+instance ToJSON Spec where toJSON = genericToJSON defaultOptions { omitNothingFields  = True }
+instance ToJSON MatchLabels where toJSON = genericToJSON defaultOptions { omitNothingFields  = True }
+instance ToJSON Selector where toJSON = genericToJSON defaultOptions { omitNothingFields  = True }
+
