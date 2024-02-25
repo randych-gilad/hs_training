038eff200648ab1993a90a4c0d2ea7c7f63fd5a4
Randych <59070097+randych-gilad@users.noreply.github.com>
2024-02-25 05:19:21 +0200

    head/body wrapper

--- html-blog/app/Html.hs
+++ html-blog/app/Html.hs
@@ -1,4 +1,8 @@
 module Html where
 
-wrapHtml :: String -> String
-wrapHtml content = "<html><body>" <> content <> "</body></html>"
+wrapHtml :: String -> String -> String
+wrapHtml contentHead contentBody = "<html>" <> wrapHead contentHead <> wrapBody contentBody <> "</html>"
+wrapHead :: String -> String
+wrapHead content = "<head>" <> content <> "</head>"
+wrapBody :: String -> String
+wrapBody content = "<body>" <> content <> "</body>"
--- html-blog/app/Main.hs
+++ html-blog/app/Main.hs
@@ -3,4 +3,4 @@ module Main where
 import qualified Html
 
 main :: IO ()
-main = putStrLn $ Html.wrapHtml "Kekus..."
+main = putStrLn $ Html.wrapHtml "Kekus page" "Kekus..."
